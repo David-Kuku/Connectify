8d9ffcc478e7e78be5f42e204232f87cbc7643ca
David-Kuku <psalmsgus99@gmail.com>
2020-07-14 12:56:20 +0100

    Updates

---
+++ precache-manifest.305e34f2cbcd46ca3a7220eeb3ee3348.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935e01deb8f750feb80178317ee1352d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4627f164ebab3c19c1f3",
+    "url": "/static/css/2.531657b8.chunk.css"
+  },
+  {
+    "revision": "ba1c0b2efe619ed0c0d6",
+    "url": "/static/css/main.f876910e.chunk.css"
+  },
+  {
+    "revision": "4627f164ebab3c19c1f3",
+    "url": "/static/js/2.4e3e6752.chunk.js"
+  },
+  {
+    "revision": "062222f47ec86680093a3a817014f059",
+    "url": "/static/js/2.4e3e6752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1c0b2efe619ed0c0d6",
+    "url": "/static/js/main.7d0e3912.chunk.js"
+  },
+  {
+    "revision": "81c418597512d6089547",
+    "url": "/static/js/runtime-main.1e3ecfa2.js"
+  },
+  {
+    "revision": "474b7a6d82a6c2db8b0c337612c9ebaf",
+    "url": "/static/media/auto1.474b7a6d.jpg"
+  }
+]);
